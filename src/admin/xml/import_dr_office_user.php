c3d3150957670cf68de798ab6eba4c525eb2b943
thevictorgreen <thevictorgreen@gmail.com>
2017-11-28 13:58:50 -0500

    added infile permission via pdo

--- src/admin/xml/import_dr_office_user.php
+++ src/admin/xml/import_dr_office_user.php
@@ -6,7 +6,7 @@
   $db_name = getenv('DB_NAME');
   $db_user = getenv('DB_USER');
   $db_pass = getenv('DB_PASS');
-  $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
+  $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass,array(PDO::MYSQL_ATTR_LOCAL_INFILE => true));
   // DATABASE CONNECTION CODE
 
   $id = 1;//$_GET['id'];
