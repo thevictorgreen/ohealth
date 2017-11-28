0901a65cc794462dca97553e26c8f368e22daf5f
thevictorgreen <thevictorgreen@gmail.com>
2017-11-28 02:41:52 -0500

    dbcode tbs

--- src/admin/xml/view_dr_office.php
+++ src/admin/xml/view_dr_office.php
@@ -2,19 +2,14 @@
         require("../codebase/connector/grid_connector.php");
         require("../codebase/connector/db_pdo.php");
 
-        //$conn=mysql_connect("localhost","root","g0th@m");
-        //mysql_select_db("OPENHEALTH");
-        //$data = new GridConnector($conn,"MySQL");
-        //$data->render_table("gl_droffice","id","id,name,addr,city,state,zip,phone");
-
         // DATABASE CONNECTION CODE
         $db_host = getenv('DB_HOST');
         $db_name = getenv('DB_NAME');
         $db_user = getenv('DB_USER');
         $db_pass = getenv('DB_PASS');
-        $db = new PDO('mysql:host=$db_host;dbname=$db_name;charset=utf8mb4', '$db_user', '$db_pass');
+        //$db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
         // DATABASE CONNECTION CODE
 
-        $data = new GridConnector($db,"PDO");
-        $data->render_table("gl_droffice","id","id,name,addr,city,state,zip,phone");
+        //$data = new GridConnector($db,"PDO");
+        //$data->render_table("gl_droffice","id","id,name,addr,city,state,zip,phone");
 ?>
