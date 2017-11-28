a6637d15a2ce7ba22fe911eef965d55b85bf0c8e
thevictorgreen <thevictorgreen@gmail.com>
2017-11-28 02:43:05 -0500

    dbcode tbs

--- src/admin/xml/view_dr_office.php
+++ src/admin/xml/view_dr_office.php
@@ -7,7 +7,7 @@
         $db_name = getenv('DB_NAME');
         $db_user = getenv('DB_USER');
         $db_pass = getenv('DB_PASS');
-        //$db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
+        $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
         // DATABASE CONNECTION CODE
 
         //$data = new GridConnector($db,"PDO");
