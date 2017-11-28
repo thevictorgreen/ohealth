081f246d50c769d887e82088f2821107df551308
thevictorgreen <thevictorgreen@gmail.com>
2017-11-28 01:50:38 -0500

    updated db acces code

--- src/admin/xml/view_dr_office.php
+++ src/admin/xml/view_dr_office.php
@@ -1,11 +1,20 @@
 <?php
         require("../codebase/connector/grid_connector.php");
-        $conn=mysql_connect("localhost","root","g0th@m");
+        require("../codebase/connector/db_pdo.php");
 
-        mysql_select_db("OPENHEALTH");
+        //$conn=mysql_connect("localhost","root","g0th@m");
+        //mysql_select_db("OPENHEALTH");
+        //$data = new GridConnector($conn,"MySQL");
+        //$data->render_table("gl_droffice","id","id,name,addr,city,state,zip,phone");
 
-        $data = new GridConnector($conn,"MySQL");
+        // DATABASE CONNECTION CODE
+        $db_host = getenv('DB_HOST');
+        $db_name = getenv('DB_NAME');
+        $db_user = getenv('DB_USER');
+        $db_pass = getenv('DB_PASS');
+        $db = new PDO('mysql:host=$db_host;dbname=$db_name;charset=utf8mb4', '$db_user', '$db_pass');
+        // DATABASE CONNECTION CODE
+
+        $data = new GridConnector($db,"PDO");
         $data->render_table("gl_droffice","id","id,name,addr,city,state,zip,phone");
-
 ?>
-
