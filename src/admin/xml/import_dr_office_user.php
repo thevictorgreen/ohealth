9c55abdad95c0eca479ac7fd3d7b0279830ef82c
thevictorgreen <thevictorgreen@gmail.com>
2017-11-28 13:52:11 -0500

    updated db code

--- src/admin/xml/import_dr_office_user.php
+++ src/admin/xml/import_dr_office_user.php
@@ -1,28 +1,34 @@
 <?php
 
-  $con = mysql_connect("localhost","root","g0th@m"); //,false,MYSQL_CLIENT_SSL);
-  mysql_select_db("OPENHEALTH",$con);
+  //require_once("/GDS/ohealth/admin/xml/settings.php");
+  // DATABASE CONNECTION CODE
+  $db_host = getenv('DB_HOST');
+  $db_name = getenv('DB_NAME');
+  $db_user = getenv('DB_USER');
+  $db_pass = getenv('DB_PASS');
+  $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
+  // DATABASE CONNECTION CODE
 
   $id = 1;//$_GET['id'];
   $dr_users;
 
-  $result = mysql_query("SELECT dr_users,dr_permissions from gl_droffice where id = $id");
-  $row = mysql_fetch_array($result);
+  $result = $db->query("SELECT dr_users,dr_permissions from gl_droffice where id = $id");
+  $row = $result->fetch();
   $dr_users = $row['dr_users'];
   $dr_permissions = $row['dr_permissions'];
 
 
-  mysql_query("LOAD DATA LOCAL INFILE '../uploads/users.csv' INTO TABLE $dr_users FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' (first_name,last_name,login,passd,is_dr,dea,send_script,appr_req,email,lic_num)");
+  $db->query("LOAD DATA LOCAL INFILE '../uploads/users.csv' INTO TABLE $dr_users FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' (first_name,last_name,login,passd,is_dr,dea,send_script,appr_req,email,lic_num)");
                                                                                                                                  //(first_name,last_name,login,passd,is_dr,dea,send_script,appr_req,email,lic_num)
 
-  $result = mysql_query("SELECT id FROM $dr_users");
-  while ($row = mysql_fetch_array($result)) {
+  $result = $db->query("SELECT id FROM $dr_users");
+  while ( $row = $result->fetch() ) {
 
          $u_id = $row["id"];
-         $result2 = mysql_query("select u_id from $dr_permissions where u_id = $u_id");
+         $result2 = $db->query("select u_id from $dr_permissions where u_id = $u_id");
 
-         if (mysql_num_rows($result2) == 0) {
-             mysql_query("INSERT INTO $dr_permissions (u_id,submitScript,submitScriptDirect,submitScriptQueOnly,viewScript,viewScriptQue,viewRefills,viewAdherence,viewPatients,viewCharts,viewFormulary,viewReports) VALUES ($u_id,'false','false','false','false','false','true','true','true','true','true','true')");
+         if ( $result2->rowCount() == 0 ) {
+              $db->query("INSERT INTO $dr_permissions (u_id,submitScript,submitScriptDirect,submitScriptQueOnly,viewScript,viewScriptQue,viewRefills,viewAdherence,viewPatients,viewCharts,viewFormulary,viewReports) VALUES ($u_id,'false','false','false','false','false','true','true','true','true','true','true')");
          }
   }
 
