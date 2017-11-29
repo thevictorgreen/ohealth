defa75ac85ca92e24da5ee66728e449c748904c6
thevictorgreen <thevictorgreen@gmail.com>
2017-11-29 00:19:32 -0500

    pharmacy fax

--- src/admin/xml/view_pharmacy_fax.php
+++ src/admin/xml/view_pharmacy_fax.php
@@ -1,9 +1,16 @@
 <?php
         require("../codebase/connector/grid_connector.php");
-        $conn=mysql_connect("localhost","root","g0th@m");
+        require("../codebase/connector/db_pdo.php");
 
-        mysql_select_db("OPENHEALTH");
-        $data = new GridConnector($conn,"MySQL");
+        // DATABASE CONNECTION CODE
+        $db_host = getenv('DB_HOST');
+        $db_name = getenv('DB_NAME');
+        $db_user = getenv('DB_USER');
+        $db_pass = getenv('DB_PASS');
+        $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
+        // DATABASE CONNECTION CODE
+
+        $data = new GridConnector($db,"PDO");
 
         if ($data->is_select_mode()) {
             $data->render_table("gl_pharmacy1","id","id,name,addr,city,state,fax,phone");
@@ -13,4 +20,3 @@
             $data->render_table("gl_pharmacy1","fax");
         }
 ?>
-
