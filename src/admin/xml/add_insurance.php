4b089372f258805b368d5236e1b9607a883fa1de
thevictorgreen <thevictorgreen@gmail.com>
2017-11-28 23:16:40 -0500

    add and remove ins

--- src/admin/xml/add_insurance.php
+++ src/admin/xml/add_insurance.php
@@ -1,13 +1,17 @@
 <?php
 
-  $con = mysql_connect("localhost","root","g0th@m"); //,false,MYSQL_CLIENT_SSL);
-  mysql_select_db("OPENHEALTH",$con);
+  // DATABASE CONNECTION CODE
+  $db_host = getenv('DB_HOST');
+  $db_name = getenv('DB_NAME');
+  $db_user = getenv('DB_USER');
+  $db_pass = getenv('DB_PASS');
+  $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
 
   $ins_id   = $_GET['ins_id'];
   $name = $_GET['ins_name'];
   $state = $_GET['st_short'];
 
-  mysql_query("INSERT INTO gl_insurance (ins_id,name,state) VALUES ($ins_id,'$name','$state')");
+  $db->query("INSERT INTO gl_insurance (ins_id,name,state) VALUES ($ins_id,'$name','$state')");
 
   echo "success";
 
--- src/admin/xml/remove_insurance.php
+++ src/admin/xml/remove_insurance.php
@@ -1,11 +1,15 @@
 <?php
 
-  $con = mysql_connect("localhost","root","g0th@m"); //,false,MYSQL_CLIENT_SSL);
-  mysql_select_db("OPENHEALTH",$con);
+  // DATABASE CONNECTION CODE
+  $db_host = getenv('DB_HOST');
+  $db_name = getenv('DB_NAME');
+  $db_user = getenv('DB_USER');
+  $db_pass = getenv('DB_PASS');
+  $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
 
   $ins_id   = $_GET['ins_id'];
 
-  mysql_query("DELETE FROM gl_insurance WHERE ins_id = $ins_id");
+  $db->query("DELETE FROM gl_insurance WHERE ins_id = $ins_id");
 
   echo "success";
 
