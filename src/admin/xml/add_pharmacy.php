9721b052dd07e3e4eb06d88fad4a0fe888ea6726
thevictorgreen <thevictorgreen@gmail.com>
2017-11-29 00:13:15 -0500

    add remove pharm

--- src/admin/xml/add_pharmacy.php
+++ src/admin/xml/add_pharmacy.php
@@ -1,7 +1,12 @@
 <?php
 
-  $con = mysql_connect("localhost","root","g0th@m"); //,false,MYSQL_CLIENT_SSL);
-  mysql_select_db("OPENHEALTH",$con);
+  // DATABASE CONNECTION CODE
+  $db_host = getenv('DB_HOST');
+  $db_name = getenv('DB_NAME');
+  $db_user = getenv('DB_USER');
+  $db_pass = getenv('DB_PASS');
+  $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
+  // DATABASE CONNECTION CODE
 
   $id    = $_GET['id'];
   $name  = $_GET['name'];
@@ -12,7 +17,7 @@
   $lat   = $_GET['lat'];
   $lon   = $_GET['lon'];
 
-  mysql_query("INSERT INTO gl_pharmacy1 (id,name,addr,city,state,phone,lat,lng) VALUES ($id,'$name','$addr','$city','$state','$phone','$lat','$lon')");
+  $db->query("INSERT INTO gl_pharmacy1 (id,name,addr,city,state,phone,lat,lng) VALUES ($id,'$name','$addr','$city','$state','$phone','$lat','$lon')");
 
   echo "success";
 
