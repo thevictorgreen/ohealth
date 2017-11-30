9df791c5dc12d794b50c941d82cf014b9e6f5ba6
thevictorgreen <thevictorgreen@gmail.com>
2017-11-30 11:39:42 -0500

    updated database code

--- src/user/auth/authenticate.php
+++ src/user/auth/authenticate.php
@@ -1,13 +1,18 @@
 <?php
 
-  $con = mysql_connect("localhost","root","g0th@m"); //,false,MYSQL_CLIENT_SSL);
-  mysql_select_db("OPENHEALTH",$con);
+  // DATABASE CONNECTION CODE
+  $db_host = getenv('DB_HOST');
+  $db_name = getenv('DB_NAME');
+  $db_user = getenv('DB_USER');
+  $db_pass = getenv('DB_PASS');
+  $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
+  // DATABASE CONNECTION CODE
 
   $id = 1;//$_GET['id'];
   $dr_users;
 
-  $result = mysql_query("SELECT dr_users,dr_permissions from gl_droffice where id = $id");
-  $row = mysql_fetch_array($result);
+  $result = $db->query("SELECT dr_users,dr_permissions from gl_droffice where id = $id");
+  $row = $result->fetch();
   $dr_users = $row['dr_users'];
   $dr_permissions = $row['dr_permissions'];
 
@@ -16,8 +21,8 @@
   $upass = $_GET["upass"];
 
 
-  $result = mysql_query("select id from $dr_users where login='$uname' and passd='$upass'");
-  if (mysql_num_rows($result) == 1) {
+  $result = $db->query("select id from $dr_users where login='$uname' and passd='$upass'");
+  if ($result->rowCount() == 1) {
       echo "authenticated";
   }
 
